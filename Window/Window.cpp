361597566d9d43e3b172949c347f47dd8ad3553a
Paul le Roux <xonar.leroux@gmail.com>
2013-07-18 17:52:50 +0200

    Set Viewport and clear color

--- Window/Window.cpp
+++ Window/Window.cpp
@@ -24,6 +24,12 @@ int FWindow::Init()
   this->window = SDL_CreateWindow(this->title.c_str(),this->x,this->y,width,height,mode);
   this->context = SDL_GL_CreateContext(this->window);
 
+  //Set Viewport
+  glViewport( 0.f, 0.f, this->width, this->height);
+
+  //Initialize Clear Color
+  glClearColor( 0.f, 0.f, 0.f, 1.f );
+
   return 0;
 }
 
