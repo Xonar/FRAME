009c05b4f67b75a880ce5f30d84bb748539cb510
Paul le Roux <xonar.leroux@gmail.com>
2013-07-31 16:45:52 +0200

    Add extended file functionality

---
+++ Lib/Files.cpp
@@ -0,0 +1,40 @@
+/*
+ * File :
+ *      ./Lib/Files.cpp
+ *
+ * Author :
+ *      Xonar <Paul le Roux>
+ *
+ * Description:
+ *      Definitions of ./Lib/Files.h
+ */
+
+#include "Files.h"
+
+#include <fstream>
+#include <iostream>
+
+std::string readFileIntoString(std::string path)
+{
+  std::ifstream in(path, std::ios::in | std::ios::binary );
+
+  if(in)
+  {
+    std::string out;
+
+    in.seekg(0, std::ios::end);
+    out.resize(in.tellg());
+
+    in.seekg(0, std::ios::beg);
+    in.read(&out[0], out.size());
+
+    in.close();
+
+    return out;
+  }
+  else
+  {
+    std::cerr << "Failed reading file : \"" << path << "\"" << std::endl;
+    return "";
+  }
+}
---
+++ Lib/Files.h
@@ -0,0 +1,19 @@
+/*
+ * File :
+ *      ./Lib/Files.h
+ *
+ * Author :
+ *      Xonar <Paul le Roux>
+ *
+ * Description:
+ *      Contains high-level functions for accessing files
+ */
+
+#ifndef _F_H_FILES_
+#define _F_H_FILES_
+
+#include <string>
+
+std::string readFileIntoString(std::string path);
+
+#endif // _F_H_FILES_
