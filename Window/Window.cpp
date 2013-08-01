a840d372c66e25918bb5c348255a72c67557e76e
Paul le Roux <xonar.leroux@gmail.com>
2013-08-01 19:16:06 +0200

    Check if at least OpenGL 3.3 is supported

--- Window/Window.cpp
+++ Window/Window.cpp
@@ -23,18 +23,53 @@ FWindow::FWindow(std::string title)
 
 int FWindow::Init()
 {
+  //Create Window
   this->window = SDL_CreateWindow(this->title.c_str(),this->x,this->y,width,height,mode);
+
+  //Create Dummy Context
   this->context = SDL_GL_CreateContext(this->window);
+ 
+  //Get OpenGL Version
+  const GLubyte* sGLVersion =  glGetString(GL_VERSION);
+
+  this->glMajorVersion = (sGLVersion[0]-'0')*10;
+  this->glMajorVersion += (sGLVersion[2]-'0');
+  
+  //Desotry Context as its not need any more
+  SDL_GL_DeleteContext(this->context);
+
+  if(this->glMajorVersion >= 41)
+  {
+    this->glMajorVersion = 4;
+    this->glMinorVersion = 2;
+  }
+  else if(this->glMajorVersion >= 33)
+  {
+    this->glMajorVersion = 3;
+    this->glMinorVersion = 3;
+  }
+  else
+  {
+    std::cerr << "Can't create OpenGL 3.3+ Context! ( Can only create " 
+              << glMajorVersion/10 << "." << glMajorVersion%10 << ")" << std::endl;
+
+    //Exit
+    SDL_DestroyWindow( this->window);
+    
+    return 1;
+  }
+
+  //Create Context
+  SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, this->glMajorVersion );
+  SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, this->glMinorVersion );
+
+  this->context = SDL_GL_CreateContext( this->window);
 
   //Set Viewport
   glViewport( 0.f, 0.f, this->width, this->height);
 
   //Initialize Clear Color
   glClearColor( 0.f, 0.f, 0.f, 1.f );
-
-  //Get GL Variables
-  SDL_GL_GetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, &this->glMajorVersion );
-  SDL_GL_GetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, &this->glMinorVersion );
 
   //Start Message
   std::cout << "Intialized Window : \"" << title << "\"" << std::endl;
