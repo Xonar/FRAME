4c3acfd5c75e370db8733957f1edb8b1d51bbbec
Paul le Roux <xonar.leroux@gmail.com>
2013-08-06 16:02:13 +0200

    Add missing Containers

---
+++ Container/Container.h
@@ -0,0 +1,31 @@
+/*
+ * File :
+ *      ./Container/Container.h
+ *
+ * Author :
+ *      Xonar <Paul le Roux>
+ *
+ * Description:
+ *      All Containers used by FRAME
+ */
+
+#ifndef _F_H_CONTAINER_
+#define _F_H_CONTAINER_
+
+#include <glm/glm.hpp>
+
+struct FTextVertex
+{
+  glm::vec3 pos;
+  glm::vec3 tex;
+};
+
+struct FVertex
+{
+  glm::vec3 pos;
+  glm::vec3 col;
+  glm::vec2 tex;
+  glm::vec3 nor;
+};
+
+#endif //_F_H_CONTAINER
