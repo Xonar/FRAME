5441dee60771fb6a8f74b5534b8544a8515bc5ec
Paul le Roux <xonar.leroux@gmail.com>
2013-09-14 18:41:08 +0200

    Remove deprecated test function from FTexture

--- Texture/Texture.cpp
+++ Texture/Texture.cpp
@@ -81,24 +81,3 @@ int FTexture::loadTextureFromFile(std::string file)
     return 1;
   }
 }
-
-//TEST: This function is used to test the image load capabilaties and should NOT be used
-//TODO: Remove after implementing sufficient other functions/methods/classes
-
-int FTexture::blitToScreen(glm::vec2 pos)
-{
-  glLoadIdentity();
-  glTranslatef( pos.x, pos.y, 0.f );
-
-  glBindTexture (GL_TEXTURE_2D, this->glTexture);
-
-  glBegin( GL_QUADS );
-    glColor3f(1.f,1.f,1.f);
-    glTexCoord2f( 0.f, 0.f ); glVertex2f( 0.f, 0.f ); 
-    glTexCoord2f( 1.f, 0.f ); glVertex2f( this->width, 0.f );
-    glTexCoord2f( 1.f, 1.f ); glVertex2f( this->width, this->height );
-    glTexCoord2f( 0.f, 1.f ); glVertex2f( 0.f, this->height );
-  glEnd();
-
-  return 0;
-}
