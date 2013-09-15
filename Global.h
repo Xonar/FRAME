a031859a35f23378b9969fe9cc2d53fba281257f
Paul le Roux <xonar.leroux@gmail.com>
2013-09-15 12:39:03 +0200

    Add Global Window Pointer

--- Global.h
+++ Global.h
@@ -13,6 +13,7 @@
 #define _F_H_GLOBAL_
 
 #include <SDL2/SDL_opengl.h>
+#include "Window/Window.h"
 
 //FRAME
 extern bool gGameOn;
@@ -21,5 +22,7 @@ extern bool gGameOn;
 const GLuint F_UNIFORM_BLOCK_CAMERA = 0;
 const GLuint F_UNIFORM_BLOCK_MODEL = 1;
 const GLuint F_UNIFORM_BLOCK_LIGHT = 2;
+
+extern FWindow *gWindow;
 
 #endif // _F_H_GLOBAL_
