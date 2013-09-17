cbcec1de1c1f6a320f6f417be2bf36177207277f
Paul le Roux <xonar.leroux@gmail.com>
2013-09-17 13:48:00 +0200

    Add Sampler Class (Will only be used in OpenGL 3.3 and onward)

---
+++ Texture/Sampler.h
@@ -0,0 +1,29 @@
+/*
+ * File :
+ *      ./Texture/Sampler.h
+ *
+ * Author :
+ *      Xonar <Paul le Roux>
+ *
+ * Description:
+ *      The FSampler is a handler for all samplers
+ */
+
+#ifndef _F_H_SAMPLER_
+#define _F_H_SAMPLER_
+
+#include <SDL2/SDL_opengl.h>
+
+class FSampler
+{
+private:
+  GLuint glSampler;
+ 
+public:
+  FSampler();
+  ~FSampler();
+
+  void bindSampler();
+}
+
+#endif // _F_H_SAMPLER_
