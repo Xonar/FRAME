18ec0bf85c44147243e7a618379c3636b32ba325
Paul le Roux <xonar.leroux@gmail.com>
2013-09-30 17:31:58 +0200

    Add basic instance data

---
+++ Model/ModelInstance.h
@@ -0,0 +1,45 @@
+
+/*
+ * File :
+ *      ./Model/Model.h
+ *
+ * Author :
+ *      Xonar <Paul le Roux>
+ *
+ * Description:
+ *      The FModel class is a handler for Model Objects
+ */
+
+#ifndef _F_H_MODEL_INSTANCE_
+#define _F_H_MODEL_INSTANCE_
+
+#include "../Container/Container.h"
+#include "../Texture/Texture.h"
+
+#include <SDL2/SDL_opengl.h>
+#include <string>
+
+class FModelInstance
+{
+private:
+  //Model this instance is based on
+  FModel *pModel;
+
+  //Instance specific data
+  glm::vec3 pos;
+  glm::vec3 rot;
+  glm::vec3 size;
+
+  //TODO Animation Information
+
+  //Overlay
+  FTexture *tOverlay;
+
+public:
+  FModelInstance(FModel model);
+
+  GLint readyInstance();
+}
+
+
+#endif //_F_H_MODEL_INSTANCE_
