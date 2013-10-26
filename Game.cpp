5afd07561d865ba7dbe9df075b00b9d61dcea22e
Paul le Roux <xonar.leroux@gmail.com>
2013-10-26 10:14:25 +0200

    Render Hello Font! in Game

--- Game.cpp
+++ Game.cpp
@@ -25,14 +25,20 @@
 FModel *model;
 
 FCamera *camera;
+FCamera *fontCamera;
 
 GLuint uniformViewScreenMatrix = 0;
 GLuint uniformWorldViewMatrix = 0;
+GLuint uniformOrthoMatrix = 0;
+GLuint uniformFontSampler = 0;
 
-FTexture *tex = NULL;
+FTexture2D *tex = NULL;
 GLint uniformTextureSampler = 0;
 
 FShader *shader = NULL;
+FShader *fontShader = NULL;
+
+FFont *font = NULL;
 
 static const GLfloat g_uv_buffer_data[] = {
     0.000059f, 1.0f-0.000004f,
@@ -122,28 +128,51 @@ GLint initializeGame()
 
   //Create Shader
   shader = new FShader();
+  fontShader = new FShader();
 
   shader->loadShader("Shader/FX/3D/3DForward.glvs",GL_VERTEX_SHADER);
   shader->loadShader("Shader/FX/3D/3DForward.glfs",GL_FRAGMENT_SHADER);
 
   shader->loadProgram();
 
+  fontShader->loadShader("Shader/FX/Text/text.glvs",GL_VERTEX_SHADER);
+  fontShader->loadShader("Shader/FX/Text/text.glfs",GL_FRAGMENT_SHADER);
+
+  fontShader->loadProgram();
+
   //Create Vertex Array
 
   //Create Camera
   camera = new FCamera();
+  fontCamera = new FCamera();
 
   camera->setViewPort(0,0,640,480);
   camera->setPosition(glm::vec3(4.f,3.f,-3.f));
   camera->lookAt(glm::vec3(0.f,0.f,0.f));
   camera->InitProjectionMatrix(45.f,0.1f,100.f);
 
+  fontCamera->InitOrthoMatrix(0,640,0,480);
+
   uniformViewScreenMatrix = glGetUniformLocation(shader->getProgram(), "ViewScreenMatrix" );
   uniformWorldViewMatrix = glGetUniformLocation(shader->getProgram(), "WorldViewMatrix" );
+
+  uniformOrthoMatrix = glGetUniformLocation(fontShader->getProgram(), "OrthoMatrix");
+
+  uniformFontSampler = glGetUniformLocation(fontShader->getProgram(), "Texture");
 
   //Set OpenGL Variables
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
+
+  //FFont
+  font = new FFont();
+  font->createFromTTF("Assets/TakaoPGothic.ttf", 50);
+
+  //glEnable(GL_CULL_FACE);
+  //glCullFace(GL_BACK);
+
+  glEnable (GL_BLEND);
+  glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
   return 0;
 }
@@ -161,4 +190,13 @@ GLvoid drawGame()
 
   //Draw Model
   model->draw();
+
+  //Bind Font Shader
+  fontShader->bind();
+
+  //Bind Font Camera
+  fontCamera->setMatrixUniformViewScreen(uniformOrthoMatrix);
+
+  //Draw Text
+  font->drawText("Hello Font!", glm::vec2(10,10) );
 }
